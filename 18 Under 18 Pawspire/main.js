63409ac15268af95f6c0be606be2f54ca09a752c
Sub_To_THEDOMINATOR120808 <78793579+THEDOMINATOR120808@users.noreply.github.com>
2021-03-18 22:31:58 +0530

    Add files via upload

---
+++ 18 Under 18 Pawspire/main.js
@@ -0,0 +1,31 @@
+function dog() {
+    window.location = "dog.html";
+}
+
+function cat() {
+    window.location = "cat.html";
+}
+
+function bird() {
+    window.location = "bird.html";
+}
+
+function fish() {
+    window.location = "fish.html";
+}
+
+function guinea_pig() {
+    window.location = "guinea_pig.html";
+}
+
+function hamster() {
+    window.location = "hamster.html";
+}
+
+function home() {
+    window.location = "index.html";
+}
+
+function about() {
+    window.location = "about.html";
+}
